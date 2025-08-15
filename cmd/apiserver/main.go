437d6f554beede053d292ea3faac4650cda70a1b
shikidy <129365574+shikidy@users.noreply.github.com>
2025-08-15 13:56:03 +0700

    add log lvl

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 
+	"github.com/BurntSushi/toml"
 	"github.com/shikidy/golang-rest/internal/app/apiserver"
 )
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "Set patch for config")
+}
+
 func main() {
-	s := apiserver.New()
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	if _, err := toml.DecodeFile(configPath, config); err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,11 +1,34 @@
 package apiserver
 
-type ApiServer struct{}
+import "github.com/sirupsen/logrus"
 
-func New() *ApiServer {
-	return &ApiServer{}
+type ApiServer struct {
+	config *Config
+	logger *logrus.Logger
+}
+
+func New(config *Config) *ApiServer {
+	return &ApiServer{
+		config: config,
+		logger: logrus.New(),
+	}
 }
 
 func (a *ApiServer) Start() error {
+	if err := a.configureLogger(); err != nil {
+		return err
+	}
+
+	a.logger.Info("starting api server")
+	return nil
+}
+
+func (a *ApiServer) configureLogger() error {
+	level, err := logrus.ParseLevel(a.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	a.logger.SetLevel(level)
+
 	return nil
 }
