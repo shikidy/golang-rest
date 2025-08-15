3bba9eeafea67a16906c6e3e6e7a99e99e8b0a96
shikidy <129365574+shikidy@users.noreply.github.com>
2025-08-15 15:32:13 +0700

    add testing

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 .PHONY: build
-
 build:
 	go build -v ./cmd/apiserver
+
+.PHONY: run
 run:
 	go run ./cmd/apiserver
+
+.PHONY: test
+test:
+	go test -v -race -timeout 30s ./...
 
 .DEFAULT_GOAL := build
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,16 +1,23 @@
 package apiserver
 
-import "github.com/sirupsen/logrus"
+import (
+	"io"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
 
 type ApiServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *http.ServeMux
 }
 
 func New(config *Config) *ApiServer {
 	return &ApiServer{
 		config: config,
 		logger: logrus.New(),
+		router: &http.ServeMux{},
 	}
 }
 
@@ -18,9 +25,10 @@ func (a *ApiServer) Start() error {
 	if err := a.configureLogger(); err != nil {
 		return err
 	}
+	a.configureRouter()
 
 	a.logger.Info("starting api server")
-	return nil
+	return http.ListenAndServe(a.config.BindAddr, a.router)
 }
 
 func (a *ApiServer) configureLogger() error {
@@ -31,4 +39,14 @@ func (a *ApiServer) configureLogger() error {
 	a.logger.SetLevel(level)
 
 	return nil
+}
+
+func (a *ApiServer) configureRouter() {
+	a.router.HandleFunc("/hello", a.handleHello())
+}
+
+func (a *ApiServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	}
 }
